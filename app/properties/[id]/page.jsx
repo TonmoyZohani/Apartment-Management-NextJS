21ac8cb51965f25716106784d934e90f3dfd0443
TonmoyZohani <tonmoyzohani@gmail.com>
2025-08-05 21:05:02 +0600

    server param id get

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,35 +1,39 @@
-"use client";
+// "use client";
 
-import { useParams, useRouter } from "next/navigation";
+// import {
+//   useParams,
+//   usePathname,
+//   useRouter,
+//   useSearchParams,
+// } from "next/navigation";
 
-const PropertyPage = () => {
-  const router = useRouter();
-  const params = useParams();
+// const PropertyPage = () => {
+//   const router = useRouter();
+//   const params = useParams();
+//   const searchParams = useSearchParams();
+//   const pathname = usePathname();
 
+//   console.log("Path Name", pathname);
+
+//   return (
+//     <div>
+//       <h2>Property Page {params.id}</h2>
+//       <h2>Property Page Search Params {searchParams.get("name")}</h2>
+//       {/* <button onClick={() => router.replace("/")}>Go Home</button> */}
+//     </div>
+//   );
+// };
+
+// export default PropertyPage;
+
+const PropertyPage = ({ params, searchParams }) => {
   return (
     <div>
       <h2>Property Page {params.id}</h2>
+      <h2>Property Page Search Params {searchParams.name}</h2>
       {/* <button onClick={() => router.replace("/")}>Go Home</button> */}
     </div>
   );
 };
 
 export default PropertyPage;
-
-// import { useRouter } from 'next/navigation';
-
-// export default function LoginPage() {
-//   const router = useRouter();
-
-//   const handleLogin = () => {
-//     // Replace current route in history (can't go back to login)
-//     router.replace('/dashboard');
-//   };
-
-//   return (
-//     <div>
-//       <h1>Login</h1>
-//       <button onClick={handleLogin}>Login</button>
-//     </div>
-//   );
-// }
