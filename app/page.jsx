5a5545841c499ab599039b0b96cf674f692e0d32
TonmoyZohani <tonmoyzohani@gmail.com>
2025-08-05 15:43:26 +0600

    link added

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function HomePage() {
-  return <div className="text-2xl">Home Page</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">Welcome</h1>
+      <Link href="/properties">Properties</Link>
+    </div>
+  );
 }
