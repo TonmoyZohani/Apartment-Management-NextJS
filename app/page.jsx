4339f78ad02c28ba9884dfaec32cab6ee8c7e855
TonmoyZohani <tonmoyzohani@gmail.com>
2025-08-16 16:11:10 +0600

    not found page added

--- app/page.jsx
+++ app/page.jsx
@@ -2,13 +2,15 @@ import Link from "next/link";
 import Hero from "../components/Hero";
 import InfoBoxes from "../components/InfoBoxes";
 import Footer from "../components/Footer";
+import HomeProperties from "../components/HomeProperties";
 
 export default function HomePage() {
   return (
     <div>
       <Hero />
       <InfoBoxes />
-      <Footer/>
+      <HomeProperties />
+      <Footer />
     </div>
   );
 }
