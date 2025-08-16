045aa9f372669f72ac9a43d17c5d4af46af2c620
TonmoyZohani <tonmoyzohani@gmail.com>
2025-08-16 20:58:18 +0600

    momgodb set up

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
-import Link from "next/link";
 import Hero from "../components/Hero";
 import InfoBoxes from "../components/InfoBoxes";
 import Footer from "../components/Footer";
 import HomeProperties from "../components/HomeProperties";
+import connectDB from "../config/database";
 
 export default function HomePage() {
-  console.log(process.env.MONGODB_URI);
+  // console.log(process.env.MONGODB_URI);
+
+  connectDB();
 
   return (
     <div>
