59d81323326c510fc83b97ad2ad9c373066934bb
TonmoyZohani <tonmoyzohani@gmail.com>
2025-08-16 15:28:20 +0600

    footer update

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import Hero from "../components/Hero";
 import InfoBoxes from "../components/InfoBoxes";
+import Footer from "../components/Footer";
 
 export default function HomePage() {
   return (
     <div>
       <Hero />
       <InfoBoxes />
+      <Footer/>
     </div>
   );
 }
