f0c8e2f1c4143efefea4898ce5b6b9c438d7b99b
TonmoyZohani <tonmoyzohani@gmail.com>
2025-09-01 22:11:23 +0600

    properties old code updated

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,28 @@
 // "use client";
 
+// import {
+//   useParams,
+//   usePathname,
+//   useRouter,
+//   useSearchParams,
+// } from "next/navigation";
+
+// const PropertyPage = () => {
+//   const router = useRouter();
+//   const params = useParams();
+//   const searchParams = useSearchParams();
+//   const pathname = usePathname();
+
+//   console.log("Path Name", pathname);
+
+//   return (
+//     <div>
+//       <h2>Property Page {params.id}</h2>
+//       <h2>Property Page Search Params {searchParams.get("name")}</h2>
+//       {/* <button onClick={() => router.replace("/")}>Go Home</button> */}
+//     </div>
+//   );
+// };
 
 // export default PropertyPage;
 
